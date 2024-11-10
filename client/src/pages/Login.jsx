8acba80f6d7468a9c6a083a963cb196872e89ff2
Mirian Palacios <melena0816@gmail.com>
2024-11-09 21:19:13 -0500

    started on login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,13 +1,37 @@
+import { useState } from "react";
+
 const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (e) => {
+        console.log(email)
+        console.log(password)
+        e.preventDefault();
+
+
+        fetch("/api/users/login", 
+            {
+                method: "POST",
+                body: JSON.stringify({
+                    email,
+                    password
+                })
+            }
+        )
+        .then(response => response.json())
+        .then(data => console.log(data)) 
+    }
+
     return (
         <section id="login">
             <h2>Log In</h2>
-            <form className="login-form" action="/api/users/login" method="POST">
+            <form className="login-form" onSubmit={handleSubmit}>
                 <label for="email-login">Email:</label>
-                <input type="email" id="email-login" name="email" required />
+                <input type="email" id="email-login" name="email" onChange={(e) => setEmail(e.target.value)} required />
 
                 <label for="password-login">Password:</label>
-                <input type="password" id="password-login" name="password" required />
+                <input type="password" id="password-login" name="password"  onChange={(e) => setPassword(e.target.value)} required />
 
                 <button type="submit">Log In</button>
             </form>
