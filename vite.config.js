8309456ffe7bba9fb34bf6c171358846b89c3182
Baelak <teklemichaelkaleab@gmail.com>
2024-11-17 17:31:02 -0500

    render fix - vite.config file move to root dir

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  root: 'client',  // Set client as the root directory
-  publicDir: 'public', // Ensure public folder is correctly referenced for static assets like index.html
+  //root: 'client',  // Set client as the root directory
+  //publicDir: 'public', // Ensure public folder is correctly referenced for static assets like index.html
   plugins: [react()],
 //  build: {
   //   outDir: 'dist', // Output directory for build artifacts
