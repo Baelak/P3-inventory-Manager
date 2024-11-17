e5f0a8e0ffa86853061917a4538b020df1b460c9
Baelak <teklemichaelkaleab@gmail.com>
2024-11-17 17:38:02 -0500

    updated packages

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-// File: client/vite.config.js
+// File: vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
+});
+
   //root: 'client',  // Set client as the root directory
   //publicDir: 'public', // Ensure public folder is correctly referenced for static assets like index.html
-  plugins: [react()],
 //  build: {
   //   outDir: 'dist', // Output directory for build artifacts
   //   emptyOutDir: true, // Clear the dist folder before each build
   // },
-});
