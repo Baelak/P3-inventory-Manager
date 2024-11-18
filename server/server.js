719c3a69bfa3f74719f8214d14c34e1a5489ece0
Mirian Palacios <melena0816@gmail.com>
2024-11-17 23:14:46 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -56,9 +56,9 @@ const startApolloServer = async () => {
   });
 
   if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'development') {
-    app.use(express.static(path.join(__dirname, 'client/dist')));
+    app.use(express.static(path.join(__dirname, '../client/dist')));
     app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
     });
   }
 
