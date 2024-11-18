c43df71a6363a8fbd46f3e8041667892eb796d5c
Baelak <teklemichaelkaleab@gmail.com>
2024-11-17 19:41:50 -0500

    update vite.config, index.jsx, gitignore, render

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,10 +1,12 @@
+// File: client/src/index.jsx
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Using `react-dom/client` for React 18+
+import ReactDOM from 'react-dom/client';  // Using `react-dom/client` for React 18+
 import App from './App.jsx';
 import { ApolloProvider } from '@apollo/client';
 import apolloClient from './utils/apolloClient';
-import '@/styles/app.css'; // Using alias for robust path resolution
+import './styles/app.css'
 
+// Render the application inside the div with id 'root'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ApolloProvider client={apolloClient}>
