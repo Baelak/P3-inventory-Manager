00ebfe6bb528dda98f93ede3a0cb1e81a936b64c
Baelak <teklemichaelkaleab@gmail.com>
2024-11-18 13:43:46 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -16,7 +16,6 @@ app.use(cors({
   origin: [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
-    'https://p3-inventory-manager.onrender.com/',
     'https://p3-inventory-manager-qkt8.onrender.com/',
     'https://studio.apollographql.com'  // Allow Apollo Studio
   ],
